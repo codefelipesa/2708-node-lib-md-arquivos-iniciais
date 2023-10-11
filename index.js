802624a671c15dd4b84cf6a6785f7c568d0311d9
Felipe <felipesa1411@.com>
2023-10-11 16:31:16 -0300

    ""

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 import fs from "fs"
 import chalk from "chalk";
 
-function getArchive (diretorioDoArquivo){
-    const encoding = 'utf-8'
-    fs.readFile(diretorioDoArquivo, encoding, (_, texto) => {
-        console.log(chalk.bgBlue(texto))
-    } )
-}
+// async function findArchive (diretorioDoArquivo) {
+//     const encoding = 'utf-8'
+//     fs.promises.readFile(diretorioDoArquivo,encoding).then((texto)=> console.log(chalk.green(texto))).catch(trataErro)}
 
-getArchive('./arquivos/texto.md')
+async function pegaArquivo(diretorioDoArquivo){
+    try{
+    const encoding = 'utf-8'
+    const texto = await fs.promises.readFile(diretorioDoArquivo,encoding)
+    console.log(texto)
+} catch(erro){
+    trataErro(erro)
+}}
+
+function trataErro(erro){
+     console.log(chalk.red(erro))
+     throw new Error(chalk.red(erro.code, 'Não há arquivo no diretório')) // Lançando a instância do erro
+ }
+ 
+     pegaArquivo('./arquivos/texto.md')
