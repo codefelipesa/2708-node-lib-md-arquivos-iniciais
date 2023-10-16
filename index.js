4d54a05f5184c08287c7842a20b82509efdbd1a7
Felipe <felipesa1411@.com>
2023-10-16 10:53:35 -0300

    Create new function

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 import fs from "fs"
 import chalk from "chalk";
 
-// async function findArchive (diretorioDoArquivo) {
-//     const encoding = 'utf-8'
-//     fs.promises.readFile(diretorioDoArquivo,encoding).then((texto)=> console.log(chalk.green(texto))).catch(trataErro)}
+const textoTeste = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).'
+
+function extraiLinks (texto) {
+    const regex = /\[([^[\]]*?])\((https?:\/\/[^\s?#.].[^\s,]*)\)/gm
+    const capturados = [...texto.matchAll(regex)].map(capturado => ({ // O matchAll retorna um iterador com todos os resultados da correspôndencia
+       [ capturado[1]] : [capturado[2]]
+    }))
+    console.log(capturados)
+}
+
+extraiLinks(textoTeste)
+
 
 async function pegaArquivo(diretorioDoArquivo){
     try{
@@ -19,4 +28,3 @@ function trataErro(erro){
      throw new Error(chalk.red(erro.code, 'Não há arquivo no diretório')) // Lançando a instância do erro
  }
  
-     pegaArquivo('./arquivos/texto.md')
