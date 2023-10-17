29cfb3de66c5e5acc911f2de956829e38881993c
Felipe <felipesa1411@.com>
2023-10-17 13:04:47 -0300

    script:  feat new validation

--- src/cli.js
+++ src/cli.js
@@ -14,18 +14,25 @@ async function processaTexto(argumento){
 
    try{ 
     const caminho = argumento[2]
+    const valida = argumento[3]
+
+
     if(fs.lstatSync(caminho).isFile()){
         const resultados = await pegaArquivo(argumento[2])
         imprimeLista(resultados)
+
     }else if (fs.lstatSync(caminho).isDirectory()) {
         const arquivos = await fs.promises.readdir(caminho)
         arquivos.forEach(async (nomeDeArquivo) => { // Especificamos que o forEach é Async
         const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`)
         imprimeLista(lista, nomeDeArquivo)})}
+
     } catch(erro){
+
         if(erro.code == 'ENOENT'){
             console.log(chalk.red("Não há arquivos nesse diretório, por favor verifique o diretório."))
-        }else {
+        }
+        else {
             console.log(erro)
         }
     }
