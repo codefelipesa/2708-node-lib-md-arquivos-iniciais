3e81a548a0f2be83ee4846dc7ca24af9f225ae5b
Felipe <felipesa1411@.com>
2023-10-17 12:44:22 -0300

    feat

--- src/cli.js
+++ src/cli.js
@@ -4,13 +4,16 @@ import pegaArquivo from "./index.js"
 
 const caminho = process.argv
 
-function imprimeLista(nomeDeArquivo, resultado) {
-    console.log(`${chalk.bgBlue(" Lista de links do arquivo: ")} ${chalk.yellow(nomeDeArquivo)} `,resultado)
+function imprimeLista(resultado, nomeDeArquivo = "") {
+    console.log(`${chalk.yellow(" Lista de links do arquivo: ")} 
+    ${chalk.bgBlue(nomeDeArquivo)} `,
+    resultado)
 }
 
 async function processaTexto(argumento){
 
-   try{ const caminho = argumento[2]
+   try{ 
+    const caminho = argumento[2]
     if(fs.lstatSync(caminho).isFile()){
         const resultados = await pegaArquivo(argumento[2])
         imprimeLista(resultados)
@@ -18,7 +21,7 @@ async function processaTexto(argumento){
         const arquivos = await fs.promises.readdir(caminho)
         arquivos.forEach(async (nomeDeArquivo) => { // Especificamos que o forEach é Async
         const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`)
-        imprimeLista(nomeDeArquivo, lista)})}
+        imprimeLista(lista, nomeDeArquivo)})}
     } catch(erro){
         if(erro.code == 'ENOENT'){
             console.log(chalk.red("Não há arquivos nesse diretório, por favor verifique o diretório."))
