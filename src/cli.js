39f51e73b89a0f16ecbdfd06f06c03fa1457f4c6
Felipe <felipesa1411@.com>
2023-10-17 12:10:38 -0300

    feat custom error

--- src/cli.js
+++ src/cli.js
@@ -4,22 +4,27 @@ import pegaArquivo from "./index.js"
 
 const caminho = process.argv
 
+function imprimeLista(nomeDeArquivo, resultado) {
+    console.log(`${chalk.bgBlue(" Lista de links do arquivo: ")} ${chalk.yellow(nomeDeArquivo)} `,resultado)
+}
 
 async function processaTexto(argumento){
-    const caminho = argumento[2]
-    
+
+   try{ const caminho = argumento[2]
     if(fs.lstatSync(caminho).isFile()){
         const resultados = await pegaArquivo(argumento[2])
-        console.log(resultados)
-    } else 
-
-    if (fs.lstatSync(caminho).isDirectory()) {
-    const arquivos = await fs.promises.readdir(caminho)
-    arquivos.forEach(async (nomeDeArquivo) => { // Especificamos que o forEach é Async
-      const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`)
-      console.log(lista, nomeDeArquivo)})
-
-    
-}
+        imprimeLista(resultados)
+    }else if (fs.lstatSync(caminho).isDirectory()) {
+        const arquivos = await fs.promises.readdir(caminho)
+        arquivos.forEach(async (nomeDeArquivo) => { // Especificamos que o forEach é Async
+        const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`)
+        imprimeLista(nomeDeArquivo, lista)})}
+    } catch(erro){
+        if(erro.code == 'ENOENT'){
+            console.log(chalk.red("Não há arquivos nesse diretório, por favor verifique o diretório."))
+        }else {
+            console.log(erro)
+        }
+    }
 }
 processaTexto(caminho)
